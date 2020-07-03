703d0bd8e90f10a4b4a234d9a49cae1e0c15e59d
Faris Huskovic <fhuskovic92@gmail.com>
2020-07-03 01:25:27 -0400

    implement replay server; open browser to server; serve recording

--- cmd/record.go
+++ cmd/record.go
@@ -3,12 +3,18 @@ package cmd
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"fmt"
 	"image/jpeg"
+	"net/http"
 	"os"
 	"os/exec"
+	"os/signal"
+	"runtime"
+	"syscall"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/icza/mjpeg"
 	"github.com/kbinani/screenshot"
 	"github.com/spf13/pflag"
@@ -16,7 +22,12 @@ import (
 	"go.coder.com/flog"
 )
 
-type recordCmd struct{ name string }
+var signals = []os.Signal{syscall.SIGINT, syscall.SIGTERM}
+
+type recordCmd struct {
+	name string
+	port int
+}
 
 // Spec returns a command spec containing a description of it's usage.
 func (cmd *recordCmd) Spec() cli.CommandSpec {
@@ -29,6 +40,7 @@ func (cmd *recordCmd) Spec() cli.CommandSpec {
 
 // RegisterFlags initializes how a flag set is processed for a particular command.
 func (cmd *recordCmd) RegisterFlags(fl *pflag.FlagSet) {
+	fl.IntVarP(&cmd.port, "port", "p", cmd.port, "Port to run the replay server on.")
 	fl.StringVarP(&cmd.name, "name", "n", cmd.name, "Name to give screen-recording.")
 }
 
@@ -36,6 +48,12 @@ func (cmd *recordCmd) RegisterFlags(fl *pflag.FlagSet) {
 func (cmd *recordCmd) Run(fl *pflag.FlagSet) {
 	if cmd.name == "" {
 		flog.Error("you forgot to name the video")
+		fl.Usage()
+		return
+	}
+
+	if cmd.port < 1 || cmd.port > 65536 {
+		flog.Error("%d is an invalid port number", cmd.port)
 		fl.Usage()
 		return
 	}
@@ -102,4 +120,69 @@ func (cmd *recordCmd) Run(fl *pflag.FlagSet) {
 	}
 
 	flog.Success("%s successfully created", out)
+	errs := make(chan error, 1)
+	port := fmt.Sprintf(":%d", cmd.port)
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, signals...)
+
+	flog.Info("starting replay server")
+	server := startReplayServer(port, out, errs)
+	defer func() {
+		if err := server.Shutdown(context.Background()); err != nil {
+			flog.Error("failed to shutdown replay server", "error", err.Error())
+			return
+		}
+		println()
+		flog.Success("successfully shutdown replay server")
+	}()
+
+	flog.Info("opening browser")
+	if err := openbrowser(fmt.Sprintf("http://localhost%s", port)); err != nil {
+		flog.Fatal(err.Error())
+	}
+
+	for {
+		select {
+		case err := <-errs:
+			flog.Error(err.Error())
+			fl.Usage()
+			return
+		case <-interrupt:
+			flog.Info("stopping replay server")
+			return
+		}
+	}
+}
+
+func startReplayServer(port, out string, errs chan error) *http.Server {
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "video/mp4")
+		http.ServeFile(w, r, out)
+	})
+
+	server := &http.Server{Addr: port, Handler: r}
+
+	go func() {
+		errs <- server.ListenAndServe()
+	}()
+
+	return server
+}
+
+func openbrowser(url string) error {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform %s", runtime.GOOS)
+	}
+
+	return err
 }
